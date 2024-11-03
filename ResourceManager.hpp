fcde8fe4672f4639638bdec8b4ca606166079517
Bartek Skoniecki <b.skoniecki@interia.pl>
2024-11-03 16:33:12 +0100

    lab2_v1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,37 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+private:
+    std::unique_ptr< Resource > resource; // Zarz¹dzany wskaŸnik do Resource
+
+public:
+    // Konstruktor domyœlny - inicjalizuje zarz¹dzany obiekt
+    ResourceManager() : resource(std::make_unique< Resource >()) {}
+
+    // Konstruktor kopiuj¹cy
+    ResourceManager(const ResourceManager& other)
+        : resource(std::make_unique< Resource >(*other.resource))
+    {}
+
+    // Operator przypisania kopiuj¹cego
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) { // Unikniêcie samoprzydzia³u
+            resource = std::make_unique< Resource >(*other.resource);
+        }
+        return *this;
+    }
+
+    // Konstruktor przenosz¹cy
+    //ResourceManager(ResourceManager&& other) noexcept = default;
+
+    // Operator przypisania przenosz¹cego
+    //ResourceManager& operator=(ResourceManager&& other) noexcept = default;
+
+    // Destruktor - std::unique_ptr automatycznie zwolni zasób
+    ~ResourceManager() = default;
+
+    // Metoda get, która wywo³uje metodê get na zarz¹dzanym zasobie
+    double get() const { return resource->get(); }
+
 };
